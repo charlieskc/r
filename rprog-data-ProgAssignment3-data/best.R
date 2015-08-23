bfd9b8359a5417f921548401c0b916b9c2c51c45
charlieskc <charlieskc@gmail.com>
2015-08-24 02:33:22 +0800

    assignment #3

---
+++ rprog-data-ProgAssignment3-data/best.R
@@ -0,0 +1,39 @@
+best <- function(state, outcome) { 
+	i=0
+	if(outcome == "heart attack"){
+		i=11
+	}
+	else if(outcome == "heart failure"){
+		i=17
+	}
+	else if(outcome == "pneumonia"){
+		i=23
+	}
+	else{
+		stop("invalid outcome")
+	}
+	
+	
+
+	
+	
+		
+	df <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+	df_subset <- subset(df, df$"State"==state)
+
+	##print(df_subset)
+	if(nrow(df_subset) == 0){
+		stop("invalid state")
+	}
+	##minValue = sapply(df_subset, min)[i]
+	minValue = min(df_subset[,i])
+	print(minValue)
+		df_subset[,i] <- as.numeric(df_subset[,i])
+
+	result <- subset(df_subset, df_subset[i] <= unname(minValue))
+	##print(result);
+	result[with(result,order(Hospital.Name)), ]
+	print(result$Hospital.Name)
+
+	print(head(result,1)$Hospital.Name)
+}
